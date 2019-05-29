c5dd23033e79b0217379a753cf4a4a8950497254
Thong Nguyen <nxthongbk@gmail.com>
2019-05-29 15:02:31 +0700

    update

--- components/sensors/air/airSensor.h
+++ components/sensors/air/airSensor.h
@@ -1,6 +1,6 @@
-#ifndef ALCOHOL_SENSOR_H
-#define ALCOHOL_SENSOR_H
+#ifndef AIR_SENSOR_H
+#define AIR_SENSOR_H
 
-LE_SHARED le_result_t mangOH_ReadAlcoholSensor(double *reading);
+LE_SHARED le_result_t mangOH_ReadAirSensor(double *reading);
 
 #endif // ALCOHOL_SENSOR_H
