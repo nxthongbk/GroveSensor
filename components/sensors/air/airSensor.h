cd29e90ea11323d8b9e087992a5cd45d5c2e941f
David Frey <dfrey@sierrawireless.com>
2019-11-22 11:28:20 -0800

    airSensor: extensive refactoring to reduce duplicated code

--- components/sensors/air/airSensor.h
+++ components/sensors/air/airSensor.h
@@ -1,8 +1,11 @@
 #ifndef AIR_SENSOR_H
 #define AIR_SENSOR_H
 
-LE_SHARED le_result_t mangOH_ReadIndustrialPM1_0(double *reading);
-LE_SHARED le_result_t mangOH_ReadIndustrialPM2_5(double *reading);
-LE_SHARED le_result_t mangOH_ReadIndustrialPM10(double *reading);
+LE_SHARED le_result_t air_ReadStandardPM1_0(uint16_t *value);
+LE_SHARED le_result_t air_ReadStandardPM2_5(uint16_t *value);
+LE_SHARED le_result_t air_ReadStandardPM10(uint16_t *value);
+LE_SHARED le_result_t air_ReadAtmosphericEnvironmentPM1_0(uint16_t *value);
+LE_SHARED le_result_t air_ReadAtmosphericEnvironmentPM2_5(uint16_t *value);
+LE_SHARED le_result_t air_ReadAtmosphericEnvironmentPM10(uint16_t *value);
 
 #endif // AIR_SENSOR_H
