ebd9c270428bbc3b5948e5411d19731b0e7e8a1d
Thong Nguyen <nxthongbk@gmail.com>
2020-01-03 10:41:25 +0700

    fix indent error

--- components/sensors/speechRecognizer/speechRecognizer.c
+++ components/sensors/speechRecognizer/speechRecognizer.c
@@ -42,22 +42,19 @@ const char *voiceBuffer[] =
 
 static void SerialPortHandler(int fd, short events)
 {
-	char read_buffer[64]; /* Buffer to store the data received              */
-	int bytes_read = 0;   /* Number of bytes read by the read() system call */
-	int i = 0;
+    char read_buffer[64]; /* Buffer to store the data received              */
+    int bytes_read = 0;   /* Number of bytes read by the read() system call */
+    int i = 0;
 
-	if (events & POLLIN) // Data available to read?
-	{
+    if (events & POLLIN) // Data available to read?
+    {
         bytes_read = read(fd, &read_buffer, 10);
-		LE_INFO(read_buffer);
-
         if (bytes_read <= 0)
         {
             data_available = false;
         }
-
-		for (i = 0; i < bytes_read; i++)
-		{
+        for (i = 0; i < bytes_read; i++)
+        {
             if (read_buffer[i] >= 1 && read_buffer[i] <= sizeof(voiceBuffer))
             {
                 cmd_index = read_buffer[i] - 1;
@@ -73,25 +70,25 @@ static void SerialPortHandler(int fd, short events)
 
         LE_INFO(speech_buffer);
         io_PushString("speech_recognizer", 0, speech_buffer);
-	}
+    }
 
-	if ((events & POLLERR) || (events & POLLHUP) || (events & POLLRDHUP)) // Error or hang-up?
-	{
-		LE_ERROR("Failed to get speech recoginer");
-	}
+    if ((events & POLLERR) || (events & POLLHUP) || (events & POLLRDHUP)) // Error or hang-up?
+    {
+        LE_ERROR("Failed to get speech recoginer");
+    }
 }
 
 void speechRecognizer_Read(char* cmd, size_t id_bufferSize)
 {
-	if (id_bufferSize <= strlen(speech_buffer))
-	{
-		LE_ERROR("Input buffer did not have enough size");
-	}
+    if (id_bufferSize <= strlen(speech_buffer))
+    {
+        LE_ERROR("Input buffer did not have enough size");
+    }
 
-	memcpy(cmd,
-		   speech_buffer,
-		   strlen(speech_buffer));
-	cmd[strlen(speech_buffer)] = 0;
+    memcpy(cmd,
+           speech_buffer,
+           strlen(speech_buffer));
+    cmd[strlen(speech_buffer)] = 0;
 }
 
 COMPONENT_INIT
@@ -102,62 +99,61 @@ COMPONENT_INIT
 
     int fd = open("/dev/ttyHS0", O_RDWR|O_NONBLOCK);
     LE_FATAL_IF(fd == -1, "open failed with errno %d (%m)", errno);
-	
-	// saio.sa_mask = 0;
-	saio.sa_flags = 0;
-	saio.sa_restorer = NULL;
-	sigaction(SIGIO, &saio, NULL);
 
-	/* allow the process to receive SIGIO */
-	fcntl(fd, F_SETOWN, getpid());
-	fcntl(fd, F_SETFL, FASYNC);
+    // saio.sa_mask = 0;
+    saio.sa_flags = 0;
+    saio.sa_restorer = NULL;
+    sigaction(SIGIO, &saio, NULL);
 
-	// get the parameters
-	tcgetattr (fd, &oldtio);
-	tcgetattr (fd, &newtio);
+    /* allow the process to receive SIGIO */
+    fcntl(fd, F_SETOWN, getpid());
+    fcntl(fd, F_SETFL, FASYNC);
 
-	// Set the baud rates to 9600...
-	cfsetispeed(&newtio, B9600);
-	cfsetospeed(&newtio, B9600);
-	// Enable the receiver and set local mode...
-	newtio.c_cflag |= (CLOCAL | CREAD);
-	// No parity (8N1):
-	newtio.c_cflag &= ~PARENB;
-	newtio.c_cflag &= ~CSTOPB;
-	newtio.c_cflag &= ~CSIZE;
-	newtio.c_cflag |= CS8;
- 	// enable hardware flow control (CNEW_RTCCTS)
-	// newtio.c_cflag |= CRTSCTS;
-	// if(hw_handshake)
-	// Disable the hardware flow control for use with mangOH
-	newtio.c_cflag &= ~CRTSCTS;
+    // get the parameters
+    tcgetattr(fd, &oldtio);
+    tcgetattr(fd, &newtio);
 
-	// set raw input
-	newtio.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-	newtio.c_iflag &= ~(INLCR | ICRNL | IGNCR);
+    // Set the baud rates to 9600...
+    cfsetispeed(&newtio, B9600);
+    cfsetospeed(&newtio, B9600);
+    // Enable the receiver and set local mode...
+    newtio.c_cflag |= (CLOCAL | CREAD);
+    // No parity (8N1):
+    newtio.c_cflag &= ~PARENB;
+    newtio.c_cflag &= ~CSTOPB;
+    newtio.c_cflag &= ~CSIZE;
+    newtio.c_cflag |= CS8;
+    // enable hardware flow control (CNEW_RTCCTS)
+    // newtio.c_cflag |= CRTSCTS;
+    // if(hw_handshake)
+    // Disable the hardware flow control for use with mangOH
+    newtio.c_cflag &= ~CRTSCTS;
 
-	// set raw output
-	newtio.c_oflag &= ~OPOST;
-	newtio.c_oflag &= ~OLCUC;
-	newtio.c_oflag &= ~ONLRET;
-	newtio.c_oflag &= ~ONOCR;
-	newtio.c_oflag &= ~OCRNL;
+    // set raw input
+    newtio.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    newtio.c_iflag &= ~(INLCR | ICRNL | IGNCR);
 
-	newtio.c_cc[VMIN] = 0;
-	newtio.c_cc[VTIME] = 0;
+    // set raw output
+    newtio.c_oflag &= ~OPOST;
+    newtio.c_oflag &= ~OLCUC;
+    newtio.c_oflag &= ~ONLRET;
+    newtio.c_oflag &= ~ONOCR;
+    newtio.c_oflag &= ~OCRNL;
 
-	// Set the new newtio for the port...
-	tcflush(fd, TCIFLUSH);
-	tcsetattr (fd, TCSANOW, &newtio);
+    newtio.c_cc[VMIN] = 0;
+    newtio.c_cc[VTIME] = 0;
 
+    // Set the new newtio for the port...
+    tcflush(fd, TCIFLUSH);
+    tcsetattr(fd, TCSANOW, &newtio);
 
-	le_fdMonitor_Create("Serial Port",
-						fd,				   
-						SerialPortHandler,
-						POLLIN);		  
+    le_fdMonitor_Create("Serial Port",
+                        fd,
+                        SerialPortHandler,
+                        POLLIN);
 
-	LE_FATAL_IF(io_CreateInput("speech_recognizer",
-								IO_DATA_TYPE_STRING,
-								"string") != LE_OK,
-				"FAILED to create input speech_recoginer");
+    LE_FATAL_IF(io_CreateInput("speech_recognizer",
+                               IO_DATA_TYPE_STRING,
+                               "string") != LE_OK,
+                "FAILED to create input speech_recoginer");
 }
